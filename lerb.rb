c6ea9bce1c91e2130226d7993dfe7f707e647c1a
Chad Hendry <chendry@chendry.org>
2015-12-25 14:25:10 -0600

    moving LERB class to top

--- lerb.rb
+++ lerb.rb
@@ -7,6 +7,49 @@ require 'json'
 require 'base64'
 require 'net/http'
 
+class LERB
+  def initialize(uri, key)
+    @uri = URI(uri)
+    @key = OpenSSL::PKey::RSA.new(File.read(key))
+  end
+
+  def run
+    new_registration("email@example.com")
+  end
+
+  def new_registration(email)
+    execute URI(directory["new-reg"]),
+      "resource": "new-reg",
+      "contact": [ "mailto:#{email}" ]
+  end
+
+  private
+
+    def directory
+      @directory ||= JSON.parse(open(@uri.to_s).read)
+    end
+
+    def execute(uri, payload)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.set_debug_output(STDOUT)
+
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.body = JWS.new(@key, nonce, payload.to_json).build
+
+      http.request(request)
+    end
+
+    def nonce
+      http = Net::HTTP.new(@uri.host, @uri.port)
+      http.use_ssl = true
+
+      http.start do |h|
+        h.request(Net::HTTP::Head.new(@uri))["Replay-Nonce"]
+      end
+    end
+end
+
 class JWS
   def initialize(key, nonce, payload)
     @key = key
@@ -46,49 +89,6 @@ class JWS
 
     def number_to_bytes(n)
       n.to_s(16).scan(/../).collect(&:hex).pack("C*")
-    end
-end
-
-class LERB
-  def initialize(uri, key)
-    @uri = URI(uri)
-    @key = OpenSSL::PKey::RSA.new(File.read(key))
-  end
-
-  def run
-    register("email@example.com")
-  end
-
-  def register(email)
-    execute URI(directory["new-reg"]),
-      "resource": "new-reg",
-      "contact": [ "mailto:#{email}" ]
-  end
-
-  private
-
-    def directory
-      @directory ||= JSON.parse(open(@uri.to_s).read)
-    end
-
-    def execute(uri, payload)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.set_debug_output(STDOUT)
-
-      request = Net::HTTP::Post.new(uri.request_uri)
-      request.body = JWS.new(@key, nonce, payload.to_json).build
-
-      http.request(request)
-    end
-
-    def nonce
-      http = Net::HTTP.new(@uri.host, @uri.port)
-      http.use_ssl = true
-
-      http.start do |h|
-        h.request(Net::HTTP::Head.new(@uri))["Replay-Nonce"]
-      end
     end
 end
 
