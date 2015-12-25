11c455484119944a07ce23c9de4b7bb8a4c14870
Chad Hendry <chendry@chendry.org>
2015-12-25 14:17:26 -0600

    adding JWS signature support

--- lerb.rb
+++ lerb.rb
@@ -4,6 +4,49 @@ require 'uri'
 require 'openssl'
 require 'open-uri'
 require 'json'
+require 'base64'
+
+class JWS
+  def initialize(key, nonce, payload)
+    @key = key
+    @nonce = nonce
+    @payload = payload
+  end
+
+  def build
+    "#{signing_input}.#{signature}"
+  end
+
+  private
+
+    def signing_input
+      b64(header) + "." + b64(@payload)
+    end
+
+    def signature
+      b64(@key.sign(OpenSSL::Digest::SHA256.new, signing_input))
+    end
+
+    def b64(bin)
+      Base64.urlsafe_encode64(bin).gsub(/=+$/, "")
+    end
+
+    def header
+      {
+        alg: "RS256",
+        nonce: @nonce,
+        jwk: {
+          kty: "RSA",
+          n: b64(number_to_bytes(@key.params["n"])),
+          e: b64(number_to_bytes(@key.params["e"]))
+        }
+      }.to_json
+    end
+
+    def number_to_bytes(n)
+      n.to_s(16).scan(/../).collect(&:hex).pack("C*")
+    end
+end
 
 class LERB
   def initialize(uri, key)
@@ -12,7 +55,7 @@ class LERB
   end
 
   def run
-    puts directory.inspect
+		puts JWS.new(@key, "1234", "payload").build
   end
 
   private
